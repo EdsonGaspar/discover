8417b785019153ea2525a68eda84ef750c4bdef2
Edson Gaspar <114083090+EdsonGaspar@users.noreply.github.com>
2023-10-03 16:54:37 +0100

    Minimização de codigo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # discover
  Fazendo o curso da Rocketseat Discover
+Bem esta aplicação é consertrada em criar um ambiente mas permitindo que elá tenha dois temas um Dark e um light.
+A codificação do JS é basicamente consentrado nisso.
--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 function toggleMode(){
-  //alert(`Oi!`)
   const html = document.documentElement
+  /** 
+   * Está logica pode ser substituida pelo (toggle)
   if(html.classList.contains('light')){
     html.classList.remove('light')
   }else{
     html.classList.add('light')
   }
+  */
+ html.classList.toggle('light')
 }
